cb995cdff716a221a3a917da4a718e2546cf1445
Rayven <rayven.burns@gmail.com>
2020-02-19 18:52:25 -0500

    initial commit

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,7 +3,17 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  #find max difference between smallest and largest prices, so we should loop through the array
+
+  for i in range(0, len(prices)-1):
+    price = prices[i]
+
+
+    return maxprofit
+
+  #max profit = some price by another price that comes before it (it can't come after it)
+
+print(find_max_profit([1050, 270, 1540, 3800, 2]))
 
 
 if __name__ == '__main__':

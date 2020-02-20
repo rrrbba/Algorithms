8077a19c3fd985314e8968eb83ffd4d2c31adf64
Rayven <rayven.burns@gmail.com>
2020-02-20 14:17:14 -0500

    stock prices and eating cookies done

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,8 +5,20 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
-def eating_cookies(n, cache=None):
-  pass
+#rtc = O(n)
+def eating_cookies(n, cache={0:0, 1:1, 2:2, 3:4}): #input: fib number
+  if n < 0: 
+    return 0
+  if n == 0:
+    return 1
+  elif cache and n in cache:
+    return cache[n]
+  else:
+    cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+ 
+  return cache[n]
+
+# print(eating_cookies(35))
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,17 +3,27 @@
 import argparse
 
 def find_max_profit(prices):
-  #find max difference between smallest and largest prices, so we should loop through the array
 
-  for i in range(0, len(prices)-1):
-    price = prices[i]
+  minPrice = prices[0] #min price set to first number in array
+
+  maxProfit = prices[1] - prices[0] #max profit = second number - first number(min number at the moment)
+
+  for i in range(1, len(prices)): #starts at one -> length of array (starting at zero gives me 0)
+
+    if prices[i] < minPrice: #if price at the current index is smaller than the min price that we start with
+
+      minPrice = prices[i] #the new min price is that price at current index, keeps the lowest price to the left of the max price
+
+    elif prices[i] - minPrice > maxProfit: #instead of looking for max price using the max(), we need to find the the number that gives us the largest difference when subtracted from the min price
+
+      maxProfit = prices[i] - minPrice #max profit = max price - min price
 
 
-    return maxprofit
 
-  #max profit = some price by another price that comes before it (it can't come after it)
+  return maxProfit
 
-print(find_max_profit([1050, 270, 1540, 3800, 2]))
+
+print(find_max_profit([100, 90, 80, 50, 20, 10]))
 
 
 if __name__ == '__main__':

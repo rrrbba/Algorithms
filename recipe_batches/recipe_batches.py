790a87e5da3cc85ef3840f4dfd1dcd6ba3261a12
Rayven <rayven.burns@gmail.com>
2020-02-24 19:31:37 -0500

    pushing complete work

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,7 +3,31 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  batches = 1000
+  
+  # if batches == 0: 
+  #   return 0 
+  for a in ingredients.keys():
+    #if ingredient isn't in the recipe ingre
+    # print(a, ingredients)
+    if a not in ingredients:
+      
+      return 0
+    if ingredients[a] > recipe[a]:
+      quantity = ingredients[a] // recipe[a]
+      if quantity < batches:
+        batches = quantity
+    
+    # elif recipe[a] < ingredients[a]:
+    #   return 0
+
+     
+  # else:
+  #   return 0
+
+  return batches
+
+print(recipe_batches({ 'milk': 2, 'sugar': 40, 'butter': 20 }, { 'milk': 5, 'sugar': 120, 'butter': 500 }))
 
 
 if __name__ == '__main__':
--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,8 +3,22 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  moves = [['rock'], ['paper'], ['scissors']]
+  movesPoss = []
 
+  def recurse(movesLeft, output): #recursive function
+    if movesLeft == 0: #once it runs out of moves left, append it to the movesPoss array
+      return movesPoss.append(output)
+    
+    for i in range(0, len(moves)): #looping through all the moves
+     recurse(movesLeft -1 , output +  moves[i]) #call function and decrement down the move(times) it has left, concat the out to the move at that position
+  
+  recurse(n, []) # starting point for the recurse function
+  
+  return movesPoss
+
+
+print(rock_paper_scissors(2))
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
